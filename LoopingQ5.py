9f19d83a691d23c38ced401de497822d5059726b
gardeharshada <gardeharshada74@gmail.com>
2026-02-12 01:15:10 +0530

    Add files via upload

---
+++ LoopingQ5.py
@@ -0,0 +1,11 @@
+# WAP to print Fibonacci series upto n.
+n = int(input("Enter number of terms:"))
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+for i in range(n):
+    print(a)
+    c = a + b
+    a = b
+    b = c
